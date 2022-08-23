9f9f85d73f14f15557d35b17e3af5eef5f2bb1be
kgitbank06 <kgitbank06@DESKTOP-Q9Q3A1V>
2022-08-23 10:39:30 +0900

    0823 before merge

--- src/main/java/com/itbank/controller/PaymentController.java
+++ src/main/java/com/itbank/controller/PaymentController.java
@@ -1,43 +1,54 @@
 
 package com.itbank.controller;
 
+import java.io.IOException;
 
-import java.util.HashMap;
+import java.util.Locale;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.servlet.http.HttpSession;
+
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
-import com.itbank.info.InfoDTO;
-import com.itbank.room.RoomDTO;
-import com.itbank.service.ReservationService;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+
+import com.siot.IamportRestClient.IamportClient;
+import com.siot.IamportRestClient.exception.IamportResponseException;
+import com.siot.IamportRestClient.response.IamportResponse;
+import com.siot.IamportRestClient.response.Payment;
 
 
 @Controller
 @RequestMapping("/")
 public class PaymentController {
-   
-   @Autowired ReservationService rsvnService;
-   //예약 관련 컨트롤러
-//   @GetMapping("rsvn/reservation")
-//   public void reservation() {//@PathVariable int idx
-//   }
-   
-    @GetMapping("rsvn/reservation") //idx = room_idx
-    public ModelAndView reservationReady(@RequestParam HashMap<String,String> param){
-        ModelAndView mav = new ModelAndView("rsvn/reservation");
-    
-        RoomDTO roomDto = rsvnService.checkRoom(param);
-        InfoDTO infoDto = rsvnService.checkProduct(param);
-        mav.addObject("roomDto",roomDto);
-        mav.addObject("infoDto",infoDto);
-        return mav;  
-     }
-    
+	
+	 private IamportClient iamportClient;
+	
+
+	 //생성자를 통해 Rest API / Rest API secret 입력
+	 public PaymentController() {
+		 String apiKey = "1078805824161077";
+		 String apiSecret = "gbbvMujkPRGQBTEXdVWfPL7mNPiTO63VQBcb2qrpsfnaspipd2KYdnSxzEEqUrkqyQgEKigwAJCARppz";
+		 this.iamportClient = new IamportClient(apiKey, apiSecret);
+	 }
+	 
+	  //프론트에서 넘겨 받은 PG사 결과값을 통해 iamport token 발행
+	  @ResponseBody
+	  @PostMapping(value="rsvn/reservation/verifyIamport/{imp_uid}")
+	  public IamportResponse<Payment> paymentByImpUid(
+			 Locale locale,
+			 Model model,
+			 HttpSession httpSession,
+			 @PathVariable(value="imp_uid") String imp_uid) throws IamportResponseException, IOException{
+		  return iamportClient.paymentByImpUid(imp_uid);
+	  }
 
 }
 
