202bd5014976c4316699e1b03cc60628e2de7490
kgitbank10 <kgitbank10@DESKTOP-HKJFIN5>
2022-08-25 10:08:19 +0900

    commit!

--- src/main/java/com/itbank/controller/UserController.java
+++ src/main/java/com/itbank/controller/UserController.java
@@ -3,7 +3,6 @@ package com.itbank.controller;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
-import java.util.List;
 
 import javax.mail.MessagingException;
 import javax.servlet.http.HttpSession;
@@ -19,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.itbank.reservation.ReservationDAO;
-import com.itbank.reservation.ReservationDTO;
 
 import com.itbank.service.MailService;
 import com.itbank.service.ReservationService;
@@ -31,140 +29,149 @@ import com.itbank.user.User_sellerDTO;
 @Controller
 @RequestMapping("/user")
 public class UserController {
-	
-	@Autowired private UserService userService;
-	
-	@Autowired MailService mailservice;
-	
-	@Autowired User_nonsocialDAO nonUserDAO;
-	
-	@Autowired ReservationDAO resDAO;
-	
-	@Autowired ReservationService resService;
-	
-	@RequestMapping("/join")
-	public void join() {
-	}
+   
+   @Autowired private UserService userService;
+   
+   @Autowired MailService mailservice;
+   
+   @Autowired User_nonsocialDAO nonUserDAO;
+   
+   @Autowired ReservationDAO resDAO;
+   
+   @Autowired ReservationService resService;
+   
+   @RequestMapping("/join")
+   public void join() {
+   }
 
-	@GetMapping("/join2")
-	public String join2() {
-		return "/user/join2";
-	}
+   @GetMapping("/join2")
+   public String join2() {
+      return "/user/join2";
+   }
 
-	@PostMapping(value="/join2", produces="text/plain; charset=utf-8",consumes="text/plain; charset=utf-8")
-	@ResponseBody
-	public String join2(@RequestBody String answer) throws IOException,MessagingException{
-		String isOK = mailservice.sendConfirm(answer);
-		return isOK;
-	}
-	
-	@RequestMapping("/join3")
-	public void join3() {
-		
-	}
-	
-	@PostMapping("/join3") // login
-	public String join(User_nonsocialDTO dto) throws NoSuchAlgorithmException {
-		System.out.println(dto.getEmail());
-		System.out.println(dto.getPassword());
-		System.out.println(dto.getNickName());
-		int row = userService.join(dto);
-		System.out.println(row != 0 ? "가입 성공" : "가입 실패");
-		return "redirect:"+ "/";
-	}
-	
-	
-	@GetMapping("/login")
-	public void login() {}
-	
-	@PostMapping("/login")
-	public String login(User_nonsocialDTO dto, HttpSession session)throws NoSuchAlgorithmException {
-		System.out.println(dto.getEmail());
-		System.out.println(dto.getPassword());
-		User_nonsocialDTO login  = userService.login(dto);
-		if(login == null) {
-		
-			return "redirect:"+ "/user/relogin";
-		}
-		else {
-		session.setAttribute("login", login);
-	
-	
-		return "redirect:"+ "/";
-		}
-		
-	}
-	
-	@GetMapping("/relogin")
-	public void relogin() {}
+   @PostMapping(value="/join2", produces="text/plain; charset=utf-8",consumes="text/plain; charset=utf-8")
+   @ResponseBody
+   public String join2(@RequestBody String answer) throws IOException,MessagingException{
+      String isOK = mailservice.sendConfirm(answer);
+      return isOK;
+   }
+   
+   @RequestMapping("/join3")
+   public void join3() {
+      
+   }
+   
+   @PostMapping("/join3") // login
+   public String join(User_nonsocialDTO dto) throws NoSuchAlgorithmException {
+      System.out.println(dto.getEmail());
+      System.out.println(dto.getPassword());
+      System.out.println(dto.getNickName());
+      int row = userService.join(dto);
+      System.out.println(row != 0 ? "가입 성공" : "가입 실패");
+      return "redirect:"+ "/";
+   }
+   
+   @GetMapping("/login")
+   public void login() {}
+   
+   @PostMapping("/login")
+   public String login(User_nonsocialDTO dto, HttpSession session)throws NoSuchAlgorithmException {
+      System.out.println(dto.getEmail());
+      System.out.println(dto.getPassword());
+      User_nonsocialDTO login  = userService.login(dto);
+      if(login == null) {
+      
+         return "redirect:"+ "/user/relogin";
+      }
+      else {
+      session.setAttribute("login", login);
+   
+   
+      return "redirect:"+ "/";
+      }
+      
+   }
+   
+   @GetMapping("/relogin")
+   public void relogin() {}
 
-	
-	@GetMapping("/logout")
-	public String logout(HttpSession session) {
-		 session.invalidate();
-		return "redirect:"+ "/";
-	}
-	
-	
-	@GetMapping("/my_reservation")
-	public void my_reservation() {}
-	
-//	//마이 페이지 및 예약 내역
-//	@RequestMapping("/my_reservation")
-//	public List<MotelDTO> my_reservation() {
-//		return userService.selectList();
-//	}
-	
-	@RequestMapping("/my_reservation_detail")
-	public void my_reservation_detail() {}
-	
-	@GetMapping("/mypage")
-	public void mypage() {}
-	
-	@RequestMapping("/joindrop")
-	public void joindrop() {}
-	
-	@GetMapping("/host_join")
-	public void host_join() {}
-	
-	@PostMapping("/host_join")
-	@ResponseBody
-	public String host_join(@RequestBody HashMap<String, String> dto, HttpSession session)throws NoSuchAlgorithmException {
-		System.out.println(dto.get("email"));
-		System.out.println(dto.get("password"));
-		
-		System.out.println(dto);
-		User_sellerDTO login  = userService.seller_login(dto);
-		if(login == null) {
-			return "/user/host_join";
-		}
-		else {
-			session.setAttribute("login", login);
-			return "/user/host_home";
-		}
-	}
-	
-	
-	@PostMapping(value="/host_send", produces="text/plain; charset=utf-8",consumes="text/plain; charset=utf-8")
-	@ResponseBody
-	public String host_send(@RequestBody String answer) throws IOException,MessagingException{
-		String isOK = mailservice.sendConfirm(answer);
-		return isOK;
-	}
-	
-	
-	@RequestMapping("/host_join2")
-	public void host_join2() {}
-	
-	@RequestMapping("/host_join3")
-	public void host_join3() {}
-	
-	@GetMapping("/host_home")
-	public void host_home() {}
-	
-	
-	@RequestMapping("/host_home2")
-	public void host_home2() {}
-	
-	
+   @PostMapping("/relogin")
+   public String relogin(User_nonsocialDTO dto, HttpSession session)throws NoSuchAlgorithmException {
+      System.out.println(dto.getEmail());
+      System.out.println(dto.getPassword());
+      User_nonsocialDTO login  = userService.login(dto);
+      if(login == null) {
+      
+         return "redirect:"+ "/user/relogin";
+      }
+      else {
+      session.setAttribute("login", login);
+   
+      return "redirect:"+ "/";
+      }
+   }
+   
+   @GetMapping("/logout")
+   public String logout(HttpSession session) {
+       session.invalidate();
+      return "redirect:"+ "/";
+   }
+   
+   @GetMapping("/my_reservation")
+   public void my_reservation() {}
+   
+//   //마이 페이지 및 예약 내역
+//   @RequestMapping("/my_reservation")
+//   public List<MotelDTO> my_reservation() {
+//      return userService.selectList();
+//   }
+   
+   @RequestMapping("/my_reservation_detail")
+   public void my_reservation_detail() {}
+   
+   @GetMapping("/mypage")
+   public void mypage() {}
+   
+   @RequestMapping("/joindrop")
+   public void joindrop() {}
+   
+   @GetMapping("/host_join")
+   public void host_join() {}
+   
+   @PostMapping("/host_join")
+   @ResponseBody
+   public String host_join(@RequestBody HashMap<String, String> dto, HttpSession session)throws NoSuchAlgorithmException {
+      System.out.println(dto.get("email"));
+      System.out.println(dto.get("password"));
+      
+      System.out.println(dto);
+      User_sellerDTO login  = userService.seller_login(dto);
+      if(login == null) {
+         return "/user/host_join";
+      }
+      else {
+         session.setAttribute("login", login);
+         return "/user/host_home";
+      }
+   }
+   
+   @PostMapping(value="/host_send", produces="text/plain; charset=utf-8",consumes="text/plain; charset=utf-8")
+   @ResponseBody
+   public String host_send(@RequestBody String answer) throws IOException,MessagingException{
+      String isOK = mailservice.sendConfirm(answer);
+      return isOK;
+   }
+   
+   @RequestMapping("/host_join2")
+   public void host_join2() {}
+   
+   @RequestMapping("/host_join3")
+   public void host_join3() {}
+   
+   @GetMapping("/host_home")
+   public void host_home() {}
+   
+   @RequestMapping("/host_home2")
+   public void host_home2() {}
+   
 }
